1f58e0676828505f724d98732b0c1020247aceaf
kakkokari-gtyih <daisho7308+f@gmail.com>
2023-08-21 12:53:32 +0900

    (add) types for LocaleObject

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import genSitemap from './scripts/gen-sitemap';
+import type { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables';
 
 // 公開時のドメイン（末尾スラッシュなし）
 const baseUrl = 'https://sekigae.app';
 
-export const locales = [
+export const locales: LocaleObject[] = [
 	{ code: 'ja', iso: 'ja-JP', name: '日本語' },
 	{ code: 'en', iso: 'en-US', name: 'English' },
 ];
