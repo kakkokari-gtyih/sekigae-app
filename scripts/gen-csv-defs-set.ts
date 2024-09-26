242a0dd1dc0673ba9ff33b10e27d1bded7440151
kakkokari-gtyih <daisho7308+f@gmail.com>
2024-09-26 14:59:41 +0900

    fix: 他の言語のCSV定義でも正常にパースできるように

---
+++ scripts/gen-csv-defs-set.ts
@@ -0,0 +1,18 @@
+import { localesConst } from '../assets/data/locales';
+import path from 'path';
+import { promises as fsp } from 'fs';
+
+export async function genCSVDefsSet() {
+    // get all locales
+    const localeFilePaths = localesConst.map(locale => path.resolve(__dirname, `../locales/${locale.files[0]}`));
+
+    const locales = await Promise.all(localeFilePaths.map((localeFilePath) => fsp.readFile(localeFilePath, 'utf-8').then(JSON.parse)));
+
+    const csvHeaders = locales.map((locale) => {
+        return locale.csvSyntax?.headerIdentifier as string ?? null;
+    }).filter((header) => header !== null);
+
+    const outString = `export const csvDefsSet = ${JSON.stringify(csvHeaders)};`;
+
+    await fsp.writeFile(path.resolve(__dirname, '../assets/data/csvDefsSet.ts'), outString);
+}
