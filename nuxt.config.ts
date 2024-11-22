58ed0619989f721fb8da2bdf1cab27d6fe751320
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-11-22 10:56:49 +0900

    update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
 		strategy: 'prefix',
         trailingSlash: true,
         lazy: true,
-        langDir: 'locales',
+        langDir: './../locales',
     },
     nitro: {
 		hooks: {
