20f0160c23624a39c903952002defbe3db8342bc
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-11-22 11:02:14 +0900

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,7 +40,7 @@ export default defineNuxtConfig({
 		vueI18n: './i18n.config.ts',
 		locales,
 		defaultLocale: 'ja',
-		strategy: 'prefix',
+		strategy: 'prefix_and_default',
         trailingSlash: true,
         lazy: true,
         langDir: './../locales',
