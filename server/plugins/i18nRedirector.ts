8570c8403b4f2e3939ebafc9f9959329bc5118b8
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2024-11-22 10:57:04 +0900

    fix i18nRedirector

--- server/plugins/i18nRedirector.ts
+++ server/plugins/i18nRedirector.ts
@@ -1,4 +1,5 @@
 import { useRuntimeConfig } from '#imports';
+import { withTrailingSlash } from 'ufo';
 import type { LocaleObject } from '@nuxtjs/i18n';
 
 export default defineNitroPlugin((nitroApp) => {
@@ -9,12 +10,16 @@ export default defineNitroPlugin((nitroApp) => {
 
     const runtimeConfig = useRuntimeConfig();
     const locales = runtimeConfig.locales as LocaleObject[];
-    //@ts-ignore
-    if (!event.path.match(new RegExp(`^/(${locales.map((l) => l.code).join('|')})`))) {
+    if (!event.path.match(new RegExp(`^/(${locales.map((l) => l.code).join('|')})/`))) {
+      const links = locales.map((l) => {
+        const url = withTrailingSlash(`/${l.code}${event.path.replace(/\.html$/g, '/')}`);
+        return `<a href="${url}">${l.name}</a>`;
+      });
+
       html.htmlAttrs = [];
 
       const remainingList: string[] = [];
-      html.head.forEach((v) => {
+      html.head.forEach((v: string) => {
         remainingList.push(...(v.match(/<!--(.|\n)*(?<=-->)/gm) ?? []));
         remainingList.push(...(v.match(/<link\s+rel="(og|alternate|canonical)[^>]+>/gm) ?? []));
         remainingList.push(...(v.match(/<meta[^>]+>/gm) ?? []));
@@ -25,7 +30,10 @@ export default defineNitroPlugin((nitroApp) => {
       html.head = remainingList.map((v) => v + '\n');
       //@ts-ignore
       html.head.push('<script type="text/javascript">const s = ' + JSON.stringify(locales.map((l) => l.code)) + '; const d = Object.fromEntries(document.cookie.split(\'; \').map(v=>v.split(/=(.*)/s).map(decodeURIComponent))); if (d.i18n_redirected) { location.replace(\'/\' + d.i18n_redirected + location.pathname + location.search); } else if (s.includes(navigator.language.split("-")[0])) { location.replace(\'/\' + navigator.language.split("-")[0] + location.pathname + location.search); } else { location.replace(\'/ja\' + location.pathname + location.search); }</script>\n');
-      html.body = ['\n<noscript>Please enable Javascript to see this page properly.</noscript>\n'];
+      html.body = [
+        '\n<noscript>Please enable Javascript to see this page properly.</noscript>\n',
+        `<noscript>${links.join(', ')}</noscript>\n`,
+      ];
       html.bodyAppend = [];
       html.bodyPrepend = [];
     }
